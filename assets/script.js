cb3199c19525482f1009c7547fabc00f658c202a
n13l-tilili <niellini@gmail.com>
2025-05-23 11:51:32 +0200

    Delete assets directory

--- assets/script.js
+++
@@ -1,5 +0,0 @@
-function openTab(tabId) {
-  const tabs = document.querySelectorAll('.tabcontent');
-  tabs.forEach(tab => tab.style.display = 'none');
-  document.getElementById(tabId).style.display = 'block';
-}
